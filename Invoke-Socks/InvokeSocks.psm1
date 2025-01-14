453004f1c33257e19a0ec8f9e2e3748184b629dd
Keiran Smith <contact@keiran.scot>
2025-01-14 19:38:43 +0000

    Remove ugly comments

--- Invoke-Socks/InvokeSocks.psm1
+++ Invoke-Socks/InvokeSocks.psm1
@@ -37,7 +37,6 @@ function Invoke-Socks {
             try {
                 $clientStream = $client.GetStream()
 
-                # Step 1: Perform SOCKS5 handshake
                 $buffer = New-Object byte[] 1024
                 $bytesRead = $clientStream.Read($buffer, 0, $buffer.Length)
                 if ($bytesRead -eq 0) {
@@ -45,7 +44,6 @@ function Invoke-Socks {
                     return
                 }
 
-                # Client sends version and authentication methods
                 $clientVersion = $buffer[0]
                 if ($clientVersion -ne 0x05) {
                     Write-Host "Unsupported SOCKS version: $clientVersion"
@@ -54,11 +52,9 @@ function Invoke-Socks {
 
                 Write-Host  "SOCKS version: $clientVersion"
 
-                # Respond with "no authentication required"
                 $clientStream.WriteByte(0x05)
                 $clientStream.WriteByte(0x00)
 
-                # Step 2: Parse the SOCKS5 client request
                 $bytesRead = $clientStream.Read($buffer, 0, $buffer.Length)
                 if ($bytesRead -eq 0) {
                     Write-Host "Request parsing failed: No data received."
@@ -67,9 +63,8 @@ function Invoke-Socks {
 
                 $cmd = $buffer[1]
                 if ($cmd -ne 0x01) {
-                    # Only CONNECT (0x01) is supported
                     Write-Host "Unsupported command: $cmd"
-                    $clientStream.Write(@(0x05, 0x07, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), 0, 10) # Reply: Command not supported
+                    $clientStream.Write(@(0x05, 0x07, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), 0, 10)
                     return
                 }
 
@@ -100,31 +95,26 @@ function Invoke-Socks {
 
                 Write-Host "Request to connect to ${destinationHost}:${destinationPort}"
 
-                # Step 3: Connect to the destination server
                 $destinationClient = New-Object System.Net.Sockets.TcpClient
                 $destinationClient.Connect($destinationHost, $destinationPort)
                 $destinationStream = $destinationClient.GetStream()
 
-                # Send success response to the client
                 $response = @(
                     0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
                 )
                 $clientStream.Write($response, 0, $response.Length)
 
-                # Step 4: Relay data between client and destination server
                 $bufferSize = 8192
 
                 while ($true) {
                     $buffer = New-Object byte[] $bufferSize
                     try {
-                        # Relay client-to-server data
                         if ($clientStream.DataAvailable) {
                             $clientBytes = $clientStream.Read($buffer, 0, $bufferSize)
                             if ($clientBytes -le 0) { break }
                             $destinationStream.Write($buffer, 0, $clientBytes)
                         }
 
-                        # Relay server-to-client data
                         if ($destinationStream.DataAvailable) {
                             $serverBytes = $destinationStream.Read($buffer, 0, $bufferSize)
                             if ($serverBytes -le 0) { break }
